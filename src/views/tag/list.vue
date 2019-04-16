f455dc18fc670c306bf2e4256363a7c79faf58c1
liqiang <liqiang@weli.cn>
2019-04-16 23:51:39 +0800

    Enhancement: 标签列表

--- src/views/tag/list.vue
+++ src/views/tag/list.vue
@@ -25,22 +25,22 @@
     </el-col>
     <!--列表-->
     <el-table :data="tableList" v-loading="listLoading" border element-loading-text="拼命加载中" style="width: 100%;">
-      <el-table-column prop="uid" label="序号" width="65">
+      <el-table-column prop="id" label="序号" width="65">
       </el-table-column>
-      <el-table-column prop="cname" label="姓名">
+      <el-table-column prop="tagName" label="姓名">
       </el-table-column>
-      <el-table-column prop="title" min-width="150px" label="标题">
+      <el-table-column prop="tagDesc" min-width="150px" label="标题">
       </el-table-column>
-      <el-table-column prop="number" label="阅读数"  width="65">
+      <el-table-column prop="tagDesc" label="阅读数"  width="65">
       </el-table-column>
-      <el-table-column prop="date" label="时间" width="110px">
+      <el-table-column prop="createTime" label="时间" width="110px">
       </el-table-column>
-      <el-table-column  label="状态" width="120" >
-        <template slot-scope="scope">
-          <el-tag size="small" :type="scope.row.status | statusFilter" @click="isStatus(scope.$index, scope.row)" v-if="scope.row.status == 1">启用</el-tag>
-          <el-tag size="small"  :type="scope.row.status | statusFilter" @click="isStatus(scope.$index, scope.row)" v-if="scope.row.status == 2">禁用</el-tag>
-        </template>
-      </el-table-column>
+      <!--<el-table-column  label="updateTime" width="120" >-->
+        <!--<template slot-scope="scope">-->
+          <!--<el-tag size="small" :type="scope.row.status | statusFilter" @click="isStatus(scope.$index, scope.row)" v-if="scope.row.status == 1">启用</el-tag>-->
+          <!--<el-tag size="small"  :type="scope.row.status | statusFilter" @click="isStatus(scope.$index, scope.row)" v-if="scope.row.status == 2">禁用</el-tag>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
       <el-table-column prop="operation" label="操作 ">
         <template slot-scope="scope" >
          <el-button size="small" type="primary"  @click="handleUpdate(scope.row)">编辑</el-button>
@@ -55,7 +55,7 @@
     <!--工具条-->
     <el-pagination layout="total, prev, pager, next"
                     background
-                    :page-size="10"
+                    :page-size="2"
                     @size-change="handleSizeChange"
                     :total="total"
                     @current-change="handleCurrentChange"
@@ -106,6 +106,7 @@
 
 <script>
 import { getList, updateArticle } from '@/api/table'
+import axios from 'Axios'
 export default {
   data() {
     return {
@@ -121,7 +122,7 @@ export default {
       },
       total: 0,
       page: 1,
-      pageSize: 10,
+      pageSize: 2,
       status: [
         {
           statusId: 1,
@@ -150,14 +151,25 @@ export default {
   },
   methods: {
     fetchData() {
-      this.listLoading = true
-      getList(this.listQuery).then(response => {
-        const limit = 10
-        const pageList = response.data.filter((item, index) => index < limit * this.page && index >= limit * (this.page - 1))
-        console.log(pageList)
-        this.total = response.data.length
-        this.tableList = pageList
-        this.listLoading = false
+      // this.listLoading = true
+      // getList(this.listQuery).then(response => {
+      //   const limit = 10
+      //   const pageList = response.data.filter((item, index) => index < limit * this.page && index >= limit * (this.page - 1))
+      //   console.log(pageList)
+      //   this.total = response.data.length
+      //   this.tableList = pageList
+      //   this.listLoading = false
+      // })
+      axios.get('http://123.206.88.191:8088/manage/tags/?pageSize=' + this.pageSize + '&pageNum=' + this.page).then(result => {
+        if (result.data.status === 1000) {
+          this.tableList = result.data.data.list
+          this.total = result.data.data.total
+          this.page = result.data.data.pageNum
+          this.pageSize = result.data.data.pageSize
+          this.listLoading = false
+        } else {
+          this.$message.error(JSON.stringify(result.data.msg))
+        }
       })
     },
     doFilter() {
