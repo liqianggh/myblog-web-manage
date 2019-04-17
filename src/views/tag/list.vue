dbb6c2f0b8a35584b22c18d6c2605d3c76f78d8b
liqiang <liqiang@weli.cn>
2019-04-17 23:18:55 +0800

    Enhancement:标签修改

--- src/views/tag/list.vue
+++ src/views/tag/list.vue
@@ -4,22 +4,22 @@
       <el-form :inline="true">
         <el-form-item>
           <el-select v-model="value" clearable placeholder="状态">
-              <el-option
-                v-for="item in status"
-                :key="item.statusId"
-                :label="item.label"
-                :value="item.statusId">
-              </el-option>
+            <el-option
+              v-for="item in status"
+              :key="item.statusId"
+              :label="item.label"
+              :value="item.statusId">
+            </el-option>
           </el-select>
         </el-form-item>
-                <el-form-item >
+        <el-form-item>
           <el-input placeholder="姓名" v-model="searchName"></el-input>
         </el-form-item>
         <el-form-item>
-           <el-button type="primary" @click="doFilter()"><i class="el-icon-search"></i>搜索</el-button>
+          <el-button type="primary" @click="doFilter()"><i class="el-icon-search"></i>搜索</el-button>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" >新增</el-button>
+          <el-button type="primary">新增</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -27,24 +27,21 @@
     <el-table :data="tableList" v-loading="listLoading" border element-loading-text="拼命加载中" style="width: 100%;">
       <el-table-column prop="id" label="序号" width="65">
       </el-table-column>
-      <el-table-column prop="tagName" label="姓名">
+      <el-table-column prop="tagName" label="标题">
       </el-table-column>
-      <el-table-column prop="tagDesc" min-width="150px" label="标题">
+      <el-table-column prop="tagDesc" label="说明">
       </el-table-column>
-      <el-table-column prop="tagDesc" label="阅读数"  width="65">
+      <el-table-column prop="type" :formatter="formatType" label="类型">
       </el-table-column>
-      <el-table-column prop="createTime" label="时间" width="110px">
+      <el-table-column prop="createTime" label="创建时间" width="65">
       </el-table-column>
-      <!--<el-table-column  label="updateTime" width="120" >-->
-        <!--<template slot-scope="scope">-->
-          <!--<el-tag size="small" :type="scope.row.status | statusFilter" @click="isStatus(scope.$index, scope.row)" v-if="scope.row.status == 1">启用</el-tag>-->
-          <!--<el-tag size="small"  :type="scope.row.status | statusFilter" @click="isStatus(scope.$index, scope.row)" v-if="scope.row.status == 2">禁用</el-tag>-->
-        <!--</template>-->
-      <!--</el-table-column>-->
+      <el-table-column prop="updateTime" label="更新时间" width="110px">
+      </el-table-column>
       <el-table-column prop="operation" label="操作 ">
-        <template slot-scope="scope" >
-         <el-button size="small" type="primary"  @click="handleUpdate(scope.row)">编辑</el-button>
-         <el-button v-if="scope.row.status!='2'" size="mini" type="success" @click="handleModifyStatus(scope.row,'2')">启用
+        <template slot-scope="scope">
+          <el-button size="small" type="primary" @click="handleUpdate(scope.row)">编辑</el-button>
+          <el-button v-if="scope.row.status!='2'" size="mini" type="success" @click="handleModifyStatus(scope.row,'2')">
+            启用
           </el-button>
           <el-button v-if="scope.row.status!='1'" size="mini" @click="handleModifyStatus(scope.row,'1')">禁用
           </el-button>
@@ -54,33 +51,33 @@
     </el-table>
     <!--工具条-->
     <el-pagination layout="total, prev, pager, next"
-                    background
-                    :page-size="2"
-                    @size-change="handleSizeChange"
-                    :total="total"
-                    @current-change="handleCurrentChange"
-                    style="text-align:center;">
+                   background
+                   :page-size="this.pageSize"
+                   @size-change="handleSizeChange"
+                   :total="total"
+                   @current-change="handleCurrentChange"
+                   style="text-align:center;">
     </el-pagination>
 
 
-    <!-- 新增编辑院校 -->
+    <!-- 新增编辑标签 -->
     <el-dialog title="Edit" :visible.sync="isShowEditVisible">
-      <el-form label-width="80px" :model="temp" ref="dataForm">
-        <el-form-item label="姓名" prop="cname">
-          <el-input v-model="temp.cname"></el-input>
+      <el-form label-width="80px" :model="tagFormData" ref="dataForm">
+        <el-form-item label="标题" prop="tagName">
+          <el-input v-model="tagFormData.tagName"></el-input>
         </el-form-item>
-        <el-form-item label="时间" prop="date">
-          <el-input v-model="temp.date"></el-input>
+        <el-form-item label="详情" prop="tagDesc">
+          <el-input v-model="tagFormData.tagDesc"></el-input>
         </el-form-item>
-        <el-form-item label="状态" v-model="temp.status">
-         <el-select v-model="temp.status" placeholder="启用状态">
-            <el-option v-for="item in status"
-                       :label="item.label"
-                       :value="item.statusId"
-                       :key="item.statusId"
-            ></el-option>
-          </el-select>
-        </el-form-item>
+        <!--<el-form-item label="状态" v-model="temp.status">-->
+        <!--<el-select v-model="temp.status" placeholder="启用状态">-->
+        <!--<el-option v-for="item in status"-->
+        <!--:label="item.label"-->
+        <!--:value="item.statusId"-->
+        <!--:key="item.statusId"-->
+        <!--&gt;</el-option>-->
+        <!--</el-select>-->
+        <!--</el-form-item>-->
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="isShowEditVisible = false">取消</el-button>
@@ -103,181 +100,169 @@
 </template>
 
 
-
 <script>
-import { getList, updateArticle } from '@/api/table'
-import axios from 'Axios'
-export default {
-  data() {
-    return {
-      tableList: [],
-      listLoading: true,
-      isShowEditVisible: false,
-      deleteVisible: false,
-      temp: {
-        uid: '',
-        cname: '',
-        date: '',
-        status: ''
-      },
-      total: 0,
-      page: 1,
-      pageSize: 2,
-      status: [
-        {
-          statusId: 1,
-          label: '启用'
-        }, {
-          statusId: 0,
-          label: '禁用'
-        }
-      ],
-      value: '',
-      searchName: '',
-      filterTableDataEnd: []
-    }
-  },
-  created() {
-    this.fetchData()
-  },
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        1: 'success',
-        2: 'danger'
-      }
-      return statusMap[status]
-    }
-  },
-  methods: {
-    fetchData() {
-      // this.listLoading = true
-      // getList(this.listQuery).then(response => {
-      //   const limit = 10
-      //   const pageList = response.data.filter((item, index) => index < limit * this.page && index >= limit * (this.page - 1))
-      //   console.log(pageList)
-      //   this.total = response.data.length
-      //   this.tableList = pageList
-      //   this.listLoading = false
-      // })
-      axios.get('http://123.206.88.191:8088/manage/tags/?pageSize=' + this.pageSize + '&pageNum=' + this.page).then(result => {
-        if (result.data.status === 1000) {
-          this.tableList = result.data.data.list
-          this.total = result.data.data.total
-          this.page = result.data.data.pageNum
-          this.pageSize = result.data.data.pageSize
-          this.listLoading = false
-        } else {
-          this.$message.error(JSON.stringify(result.data.msg))
-        }
-      })
-    },
-    doFilter() {
-      if (this.searchName === '') {
-        this.fetchData()
-        // this.$message.warning('查询条件不能为空！')
-        return
-      }
-      console.log(this.searchName)
-      // 每次手动将数据置空,因为会出现多次点击搜索情况
-      this.filterTableDataEnd = []
-      this.tableList.forEach((value, index) => {
-        if (value.cname) {
-          if (value.cname.indexOf(this.searchName) >= 0) {
-            this.filterTableDataEnd.push(value)
-            console.log(this.filterTableDataEnd)
+  import axios from 'Axios'
+
+  export default {
+    data() {
+      return {
+        tableList: [],
+        listLoading: true,
+        isShowEditVisible: false,
+        deleteVisible: false,
+        tagFormData: {
+          id: 0,
+          type: 0,
+          tagName: '',
+          tagDesc: ''
+        },
+        total: 0,
+        page: 1,
+        pageSize: 5,
+        status: [
+          {
+            statusId: 1,
+            label: '启用'
+          }, {
+            statusId: 0,
+            label: '禁用'
           }
-        }
-      })
-      // 页面数据改变重新统计数据数量和当前页
-      this.page = 1
-      this.total = this.filterTableDataEnd.length
-      // 渲染表格,根据值
-      this.currentChangePage(this.filterTableDataEnd)
-    },
-    clickfun(e) {
-      console.log(e.target.innerText)
-    },
-    handleUpdate(row) {
-      this.isShowEditVisible = true
-      this.temp = Object.assign({}, row)
-      console.log(row)
-    },
-    deleteUpdate(row) {
-      console.log(row)
-      this.deleteVisible = true
-      this.temp = Object.assign({}, row)
-      // console.log(row)
-    },
-    submitDelete() {
-      const tempData = Object.assign({}, this.temp)
-      console.log(tempData)
-      console.log(this.tableList)
-      for (const v of this.tableList) {
-        if (v.uid === this.temp.uid) {
-          const index = this.tableList.indexOf(v)
-          this.tableList.splice(list, 1)
-          this.fetchData()
-          console.log(this.tableList)
-          break
-        }
+        ],
+        value: '',
+        searchName: '',
+        filterTableDataEnd: []
       }
-      this.deleteVisible = false
-      this.$notify({
-        title: '成功',
-        message: '删除成功',
-        type: 'success',
-        duration: 2000
-      })
     },
-    handleModifyStatus(row, status) {
-      this.$message({
-        message: '操作成功',
-        type: 'success'
-      })
-      console.log(row)
-      row.status = status
+    created() {
+      this.fetchData()
     },
-    updateData() {
-      const tempData = Object.assign({}, this.temp)
-      console.log(tempData)
-      updateArticle(tempData).then(() => {
+    filters: {
+      statusFilter(status) {
+        const statusMap = {
+          1: 'success',
+          2: 'danger'
+        }
+        return statusMap[status]
+      }
+    },
+    methods: {
+      fetchData() {
+        axios.get('http://123.206.88.191:8088/manage/tags?pageSize=' + this.pageSize + '&pageNum=' + this.page).then(result => {
+          if (result.data.status === 1000) {
+            this.tableList = result.data.data.list
+            this.total = result.data.data.total
+            this.page = result.data.data.pageNum
+            this.pageSize = result.data.data.pageSize
+            this.listLoading = false
+          } else {
+            this.$message.error(JSON.stringify(result.data.msg))
+          }
+        })
+      },
+      doFilter() {
+        if (this.searchName === '') {
+          this.fetchData()
+          // this.$message.warning('查询条件不能为空！')
+          return
+        }
+        console.log(this.searchName)
+        // 每次手动将数据置空,因为会出现多次点击搜索情况
+        this.filterTableDataEnd = []
+        this.tableList.forEach((value, index) => {
+          if (value.cname) {
+            if (value.cname.indexOf(this.searchName) >= 0) {
+              this.filterTableDataEnd.push(value)
+              console.log(this.filterTableDataEnd)
+            }
+          }
+        })
+        // 页面数据改变重新统计数据数量和当前页
+        this.page = 1
+        this.total = this.filterTableDataEnd.length
+        // 渲染表格,根据值
+        this.currentChangePage(this.filterTableDataEnd)
+      },
+      clickfun(e) {
+        console.log(e.target.innerText)
+      },
+      handleUpdate(row) {
+        this.isShowEditVisible = true
+        this.tagFormData = Object.assign({}, row)
+        console.log(row)
+      },
+      deleteUpdate(row) {
+        console.log(row)
+        this.deleteVisible = true
+        this.temp = Object.assign({}, row)
+        // console.log(row)
+      },
+      submitDelete() {
+        const tempData = Object.assign({}, this.temp)
+        console.log(tempData)
+        console.log(this.tableList)
         for (const v of this.tableList) {
           if (v.uid === this.temp.uid) {
             const index = this.tableList.indexOf(v)
-            this.tableList.splice(list, 1, this.temp)
+            this.tableList.splice(list, 1)
+            this.fetchData()
+            console.log(this.tableList)
             break
           }
         }
-        this.isShowEditVisible = false
+        this.deleteVisible = false
         this.$notify({
           title: '成功',
-          message: '更新成功',
+          message: '删除成功',
           type: 'success',
           duration: 2000
         })
-      })
-    },
-    handleSizeChange(val) {
-      this.page = val
-      console.log(this.page)
-      this.fetchData()
-    },
-    handleCurrentChange(val) {
-      this.page = val
-      console.log(this.page)
-      this.fetchData()
-    },
-    currentChangePage(list) {
-      let from = (this.page - 1) * this.pageSize
-      const to = this.page * this.pageSize
-      this.tableList = []
-      for (; from < to; from++) {
-        if (list[from]) {
-          this.tableList.push(list[from])
+      },
+      handleModifyStatus(row, status) {
+        this.$message({
+          message: '操作成功',
+          type: 'success'
+        })
+        console.log(row)
+        row.status = status
+      },
+      updateData() {
+        const tempData = Object.assign({})
+        tempData.id = this.tagFormData.id
+        tempData.tagName = this.tagFormData.tagName
+        tempData.tagDesc = this.tagFormData.tagDesc
+        tempData.type = this.tagFormData.type
+        axios.put('http://123.206.88.191:8088/manage/tags/' + tempData.id + '?type=' + tempData.type, tempData).then((result) => {
+          if (result.data.status === 1000) {
+            this.isShowEditVisible = false
+            this.fetchData()
+          } else {
+            this.$message.error(JSON.stringify(result.data.msg))
+          }
+        })
+      },
+      handleSizeChange(val) {
+        this.page = val
+        console.log(this.page)
+        this.fetchData()
+      },
+      handleCurrentChange(val) {
+        this.page = val
+        console.log(this.page)
+        this.fetchData()
+      },
+      currentChangePage(list) {
+        let from = (this.page - 1) * this.pageSize
+        const to = this.page * this.pageSize
+        this.tableList = []
+        for (; from < to; from++) {
+          if (list[from]) {
+            this.tableList.push(list[from])
+          }
         }
+      },
+      formatType: function(row, colum) {
+        return row.type === 1 ? '分类' : row.type === 2 ? '标签' : '未知'
       }
     }
   }
-}
 </script>
